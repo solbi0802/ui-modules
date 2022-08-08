561340171806039d7dc68bc14991bf714c320cb5
SolbiShin <sbshin.dev@gmail.com>
2022-08-08 18:57:21 +0900

    입력폼 컴포넌트 생성, 기본적인 스타일링 & 배치

---
+++ src/components/Form.tsx
@@ -0,0 +1,99 @@
+import React, { FunctionComponent, Component, useState } from 'react';
+import styled from '@emotion/styled';
+import { FormPropType, State } from 'types/FormProp.types';
+import { useEffect } from 'react';
+
+const FormWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+  border-radius: 8px;
+  width: 100%;
+`;
+
+const TextAreaSection = styled.textarea<{
+  maxLength: number;
+  state?: State;
+  defaultValue?: string;
+}>`
+  width: 100%;
+  height: 100%;
+  resize: none;
+  font-size: 16px;
+  text-align: left;
+`;
+
+const CounterTextSection = styled.span<{ contentState: State }>`
+  position: absolute;
+  border-radius: 4px;
+  font-size: 12px;
+  bottom: ${({ contentState }) =>
+    contentState === 'default' ? '13px' : '6px'};
+  right: 5px;
+`;
+
+const SaveButton = styled.button<{}>`
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 4px;
+  background-color: #ff7b30;
+  color: #ffff;
+  &:hover {
+    background-color: #f26b1d;
+  }
+`;
+
+const Form: FunctionComponent<FormPropType> = (
+  props: FormPropType,
+): JSX.Element => {
+  const { maxLength, state = 'default', defaultValue = '' } = props;
+  const [counter, setCounter] = useState(0);
+  const [contentState, setContentState] = useState(state);
+  const [isActiveButton, setIsActiveButton] = useState(false);
+
+  useEffect(() => {
+    if (defaultValue?.length > 0) {
+      setCounter(maxLength - defaultValue?.length);
+    }
+  }, []);
+
+  const calcLength = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const inputValue = e.target.value;
+
+    // 초기값과 내용이 같지 않을때만 save 버튼 활성화
+    if (defaultValue !== inputValue) {
+      setContentState('inputProgress');
+      setCounter(maxLength - inputValue.length);
+    }
+
+    setIsActiveButton(!isActiveButton);
+  };
+  return (
+    <FormWrapper>
+      <TextAreaSection
+        maxLength={maxLength}
+        onKeyUp={e => {
+          calcLength(e);
+        }}
+        readOnly={state === 'readonly'}
+        disabled={state === 'disabled'}
+        defaultValue={defaultValue}
+      ></TextAreaSection>
+      <CounterTextSection contentState={contentState}>
+        {counter}
+      </CounterTextSection>
+      {contentState === 'inputProgress' && (
+        <SaveButton
+          disabled={!isActiveButton}
+          onClick={(): void => {
+            console.log('저장되었습니다.');
+          }}
+        >
+          save
+        </SaveButton>
+      )}
+    </FormWrapper>
+  );
+};
+
+export default Form;
---
+++ src/pages/FormTemplate..tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+import Form from 'components/Form';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 68px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 16px 0 48px;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const FormTemplate: FunctionComponent = (): JSX.Element => {
+  return (
+    <Container>
+      <h2>입력 폼 UI</h2>
+      <Form maxLength={500} defaultValue={'테스트'} />
+      <Form
+        maxLength={500}
+        state={'disabled'}
+        defaultValue={'비활성화상태입니다.'}
+      />
+      <Form
+        maxLength={500}
+        state={'readonly'}
+        defaultValue={'읽기 전용 상태입니다.'}
+      />
+    </Container>
+  );
+};
+
+export default FormTemplate;
