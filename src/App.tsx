40bb257c1732bc924c2a5024219cba5848b42b97
SolbiShin <sbshin.dev@gmail.com>
2022-08-08 22:04:41 +0900

    오타 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from 'pages/Home';
 import './App.css';
 import CardTemplate from 'pages/CardTemplate';
-import FormTemplate from 'pages/FormTemplate.';
+import FormTemplate from 'pages/FormTemplate';
 
 const App = () => {
   return (
